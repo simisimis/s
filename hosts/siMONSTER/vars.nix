e691adc00f0cac807be003f4f3527a973c8f6c40
simas <simonas@narbuto.lt>
2021-12-02 23:52:49 +0100

    replace ssh keys

--- hosts/siMONSTER/vars.nix
+++ hosts/siMONSTER/vars.nix
@@ -1,6 +1,7 @@
 {
   usr.pwdHash = "$6$KIkEj/repkZ$VbCapIcC2fAp1bjHn2hAbc/UbNj1T6mXZQkclf02VDSOqgl0YHdkEY.oj5g67UItaB5UYkYAPfeKSbo3LiFBR0";
-  usr.ssh.gitea.identityFile = "~/.ssh/id_rsa_siMOHCTP_gitea";
+  usr.ssh.gitea.identityFile = "~/.ssh/id_rsa_siMONSTER_gitea";
+  usr.ssh.backute.identityFile = "~/.ssh/id_rsa_siMONSTER_backute";
   hw.hostName = "siMONSTER";
   hw.hostId = "f1b2a3d4";
   gitRepos.binfiles.ref = "simonster";
