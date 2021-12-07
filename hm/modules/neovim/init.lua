4e9957524bbcf4a5f9a4b59145c7b0a5080f2796
Simonas Narbutas <simonas@narbuto.lt>
2021-12-07 15:44:15 +0100

    who slipped this vim weasel in...

--- hm/modules/neovim/init.lua
+++ hm/modules/neovim/init.lua
@@ -5,19 +5,19 @@ if vim.opt.diff:get() then
 end -- end vimdiff theme
 
 local vimdirs = {'backup', 'tmp', 'undo'}
-local swap_dir = vim.env.HOME .. '/.cache/nvim/swap-files'
+local swap_dir = vim.env.HOME..'/.cache/nvim/swap-files'
 for _,dir in ipairs(vimdirs) do
-	if vim.fn.isdirectory(vim.env.HOME .. dir) == 0 then
-		vim.fn.mkdir(dir, 'p')
+	if vim.fn.isdirectory(vim.env.HOME.."/.cache/nvim/"..dir) == 0 then
+		vim.fn.mkdir(vim.env.HOME.."/.cache/nvim/"..dir, 'p')
 	end
 end
 vim.opt.hidden = true -- Allow buffer switching without saving
 vim.opt.backup = true -- Make a backup of the file before saving
-vim.opt.backupdir = vim.env.HOME .. '/.cache/nvim/backup' -- Directory to write backups to (should exist)
-vim.opt.directory = vim.env.HOME .. '/.cache/nvim/tmp' -- No more .sw[a-z] (swap) files all over the place (should exist)
+vim.opt.backupdir = vim.env.HOME..'/.cache/nvim/backup' -- Directory to write backups to (should exist)
+vim.opt.directory = vim.env.HOME..'/.cache/nvim/tmp' -- No more .sw[a-z] (swap) files all over the place (should exist)
 if vim.fn.has('persistent_undo') == 1 then
 	vim.opt.undofile = true -- Use persistent undo file
-	vim.opt.undodir = vim.env.HOME .. '/.cache/nvim/undo' -- Directory to write undo files to (should exist)
+	vim.opt.undodir = vim.env.HOME..'/.cache/nvim/undo' -- Directory to write undo files to (should exist)
 end
 
 vim.opt.clipboard = "unnamedplus" 
